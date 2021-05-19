9999146439ded05cded116d2e26a7163edb99c90
mvillalva <maxi.villalva@gmail.com>
2021-05-19 15:33:04 -0300

    Optimización del código

--- js/main.js
+++ js/main.js
@@ -1,58 +1,57 @@
 let width       = 0,
     nav         = document.getElementById("navi"),
     nav_button  = document.getElementById("navi__button"),
-    items       = document.getElementsByClassName('menu__item'),
+    items       = document.querySelectorAll(".menu__item"),
     menu        = document.getElementById("menu"),
     logo        = document.getElementById("logo"),
     button      = document.getElementById("button-up");
 
- width = (window.innerWidth < 768? 55:window.innerWidth >= 768 && window.innerWidth < 1024? 65:60);
+width = window.innerWidth < 768 ? 55 : window.innerWidth >= 768 && window.innerWidth < 1024 ? 65 : 60;
 
-//agrego evento en el botón menú para desplgar la lista    
-nav_button.addEventListener("click", function( event ) {    
-    if (menu.classList.contains("menu__active")) {
-        menu.classList.remove("menu__active");
-        this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-    }
-    else {
-        menu.classList.add("menu__active");
-        this.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-    }
-}, false);
+//agrego evento en el botón menú para desplgar la lista
+nav_button.addEventListener(
+    "click",
+    function () {
+        if (menu.classList.contains("menu__active")) {
+            menu.classList.remove("menu__active");
+            this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+        } else {
+            menu.classList.add("menu__active");
+            this.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+        }
+    },
+    false
+);
 
 //agrego a cada item de la lista la llamada al evento click del botón del menú
-for (var i = 0; i < items.length; i++) {
-    items.item(i).children[0].addEventListener('click', function(){
+items.forEach((element) => {
+    element.addEventListener("click", function () {
         nav_button.click();
     });
-}
+});
 
 //Efecto de scroll al presionar un item del menú
 let scrolls = document.querySelectorAll(".scroll");
-let elem, obj;
 
-for (var j = 0; j < scrolls.length; j++)
-{
-    scrolls.item(j).addEventListener("click", function(event){
+scrolls.forEach((element) => {
+    element.addEventListener("click", function (event) {
         event.preventDefault();
-
-        elem = this.getAttribute("href");
-        obj  = document.getElementById(elem.substr(1, elem.length));
+        let elem = this.getAttribute("href");
+        let obj = document.getElementById(elem.substr(1, elem.length));
 
         window.scroll({
-            top: (elem=="#"? 0: obj.offsetTop - obj.scrollTop - width),
-            behavior: 'smooth'
+            top: elem == "#" ? 0 : obj.offsetTop - obj.scrollTop - width,
+            behavior: "smooth",
         });
     });
-}
+});
 
-(function(){    
+(function () {
     button.classList.add("button-up--none");
 })();
 
 //función para cambiar clases y la selección del item menú según la posición en pantalla
-window.addEventListener('scroll', function() {
-        
+window.addEventListener("scroll", function () {
     if (document.documentElement.scrollTop == 0) {
         logo.classList.remove("navi__logo--0");
         logo.classList.add("navi__logo--25");
@@ -60,10 +59,9 @@ window.addEventListener('scroll', function() {
         nav.classList.remove("navi--shadow");
         menu.classList.remove("navi--dark");
         button.classList.add("button-up--none");
-    }
-    else{
+    } else {
         logo.classList.add("navi__logo--0");
-        logo.classList.remove("navi__logo--25");        
+        logo.classList.remove("navi__logo--25");
         nav.classList.add("navi--dark");
         nav.classList.add("navi--shadow");
         menu.classList.add("navi--dark");
@@ -73,58 +71,45 @@ window.addEventListener('scroll', function() {
             mnu_destacados  = document.getElementById("mnu_destacados"),
             mnu_nosotros    = document.getElementById("mnu_nosotros"),
             mnu_clientes    = document.getElementById("mnu_clientes"),
-            mnu_contacto    = document.getElementById("mnu_contacto"),            
+            mnu_contacto    = document.getElementById("mnu_contacto"),
             destacados      = document.getElementById("destacados"),
             clientes        = document.getElementById("clientes"),
             nosotros        = document.getElementById("nosotros"),
             contacto        = document.getElementById("contacto"),
-            items;
-        
-        if (document.documentElement.scrollTop < destacados.offsetTop - destacados.scrollTop - width){            
-            if (!mnu_inicio.parentElement.classList.contains("menu__item--select")){
-                items   = document.getElementsByClassName('menu__item');
-                for (var i = 0; i < items.length; i++){
-                    items.item(i).classList.remove("menu__item--select");
-                }
+            items           = document.querySelectorAll(".menu__item");
+
+        if (document.documentElement.scrollTop < destacados.offsetTop - destacados.scrollTop - width) {
+            if (!mnu_inicio.parentElement.classList.contains("menu__item--select")) {
+                items.forEach((element) => element.classList.remove("menu__item--select"));
                 mnu_inicio.parentElement.classList.add("menu__item--select");
             }
-        }
-        else if (document.documentElement.scrollTop >= destacados.offsetTop - destacados.scrollTop - width &&
-            document.documentElement.scrollTop < clientes.offsetTop - clientes.scrollTop - width){
-            if (!mnu_destacados.parentElement.classList.contains("menu__item--select")){
-                items   = document.getElementsByClassName('menu__item');
-                for (var i = 0; i < items.length; i++){
-                    items.item(i).classList.remove("menu__item--select");
-                }
+        } else if (
+            document.documentElement.scrollTop >= destacados.offsetTop - destacados.scrollTop - width &&
+            document.documentElement.scrollTop < clientes.offsetTop - clientes.scrollTop - width
+        ) {
+            if (!mnu_destacados.parentElement.classList.contains("menu__item--select")) {
+                items.forEach((element) => element.classList.remove("menu__item--select"));
                 mnu_destacados.parentElement.classList.add("menu__item--select");
             }
-        }
-        else if (document.documentElement.scrollTop >= clientes.offsetTop - clientes.scrollTop - width &&
-            document.documentElement.scrollTop < nosotros.offsetTop - nosotros.scrollTop - width){
-            if (!mnu_clientes.parentElement.classList.contains("menu__item--select")){
-                items   = document.getElementsByClassName('menu__item');
-                for (var i = 0; i < items.length; i++){
-                    items.item(i).classList.remove("menu__item--select");
-                }
+        } else if (
+            document.documentElement.scrollTop >= clientes.offsetTop - clientes.scrollTop - width &&
+            document.documentElement.scrollTop < nosotros.offsetTop - nosotros.scrollTop - width
+        ) {
+            if (!mnu_clientes.parentElement.classList.contains("menu__item--select")) {
+                items.forEach((element) => element.classList.remove("menu__item--select"));
                 mnu_clientes.parentElement.classList.add("menu__item--select");
             }
-        }
-        else if (document.documentElement.scrollTop >= nosotros.offsetTop - nosotros.scrollTop - width &&
-            document.documentElement.scrollTop < contacto.offsetTop - contacto.scrollTop - width){
-            if (!mnu_nosotros.parentElement.classList.contains("menu__item--select")){
-                items   = document.getElementsByClassName('menu__item');
-                for (var i = 0; i < items.length; i++){
-                    items.item(i).classList.remove("menu__item--select");
-                }
+        } else if (
+            document.documentElement.scrollTop >= nosotros.offsetTop - nosotros.scrollTop - width &&
+            document.documentElement.scrollTop < contacto.offsetTop - contacto.scrollTop - width
+        ) {
+            if (!mnu_nosotros.parentElement.classList.contains("menu__item--select")) {
+                items.forEach((element) => element.classList.remove("menu__item--select"));
                 mnu_nosotros.parentElement.classList.add("menu__item--select");
             }
-        }
-        else if (document.documentElement.scrollTop >= contacto.offsetTop - contacto.scrollTop - width){
-            if (!mnu_contacto.parentElement.classList.contains("menu__item--select")){
-                items   = document.getElementsByClassName('menu__item');
-                for (var i = 0; i < items.length; i++){
-                    items.item(i).classList.remove("menu__item--select");
-                }
+        } else if (document.documentElement.scrollTop >= contacto.offsetTop - contacto.scrollTop - width) {
+            if (!mnu_contacto.parentElement.classList.contains("menu__item--select")) {
+                items.forEach((element) => element.classList.remove("menu__item--select"));
                 mnu_contacto.parentElement.classList.add("menu__item--select");
             }
         }
